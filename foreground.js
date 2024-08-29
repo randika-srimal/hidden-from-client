6ab9d0c9d95645c938cb6eaa8b302fc4148965e5
Randika Dissanayaka <email2randika@gmail.com>
2024-08-29 22:50:44 +0530

    fix: persistency issue with comments

--- foreground.js
+++ foreground.js
@@ -26,24 +26,27 @@ const start = async () => {
         });
     }
 
-    let isFirstTime = true;
-
-
+    let initialLoading = true;
     const commentBox = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form.organization-project-tasks-task-2-route__activity-form.js-organization-project-tasks-task-2-route-activity-form > form > div.comment-field.is-avatar-shown.activity-form__comment-field > div.comment-field__text-field-container.js-comment-field-text-field-container > div > div > div > div.trix-editor-wrapper > trix-editor');
+
     commentBox.addEventListener('click', async () => {
+        const visibilityToggleButton = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form.is-field-initially-focused.organization-project-tasks-task-2-route__activity-form.js-organization-project-tasks-task-2-route-activity-form > form > div.activity-form__actions-wrapper > button');
 
-        if (isFirstTime) {
-            const visibilityToggleButton = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form.is-field-initially-focused.organization-project-tasks-task-2-route__activity-form.js-organization-project-tasks-task-2-route-activity-form > form > div.activity-form__actions-wrapper > button');
-
+        if (initialLoading) {
             visibilityToggleButton.click();
-
             const hideFromClientButton = await waitForElm('#modal-container > div > div.popover-content__body-wrapper > div > div > div > button:nth-child(2)');
-
             hideFromClientButton.click();
-        }
 
-        isFirstTime = false;
+            initialLoading = false;
+        }
     });
+
+    const commentBtn = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form.is-field-initially-focused.organization-project-tasks-task-2-route__activity-form.js-organization-project-tasks-task-2-route-activity-form > form > div.activity-form__actions-wrapper > div.form-actions.form__actions.activity-form__actions > button');
+
+    commentBtn.addEventListener('click', async () => {
+        initialLoading = true;
+    });
+
 }
 
 start();
