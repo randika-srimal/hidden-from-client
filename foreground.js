8a98cc4d6ec4000a6ce0894acad5820a604fda28
Randika Dissanayaka <email2randika@gmail.com>
2024-08-31 15:24:17 +0530

    fix: fixes and improvments

--- foreground.js
+++ foreground.js
@@ -47,3 +47,5 @@ chrome.runtime.onMessage.addListener((request) => {
     console.log(request);
     start();
 });
+
+start();
--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,8 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-      if (changeInfo.status === 'complete' && tab.url.includes('app.productive.io')) {
-        chrome.tabs.sendMessage(tabId, {
-          message: 'ProductiveTabUpdated'
-        });
-      }
-    })
-  });
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete' && tab.url.includes('app.productive.io')) {
+    chrome.tabs.sendMessage(tabId, {
+      message: 'ProductiveTabUpdated'
+    });
+    console.log('ProductiveTabUpdated Message Sent');
+  }
+});
