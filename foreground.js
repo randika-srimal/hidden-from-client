eda311d31a901771d58c2c6cea3c13412971f9eb
Randika Dissanayaka <email2randika@gmail.com>
2025-04-01 23:34:48 +0530

    fix: issue with not setting the comment as hidden

--- foreground.js
+++ foreground.js
@@ -1,31 +1,29 @@
-const start = async () => {
-    function waitForElm(selector) {
-        return new Promise(resolve => {
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
             if (document.querySelector(selector)) {
-                return resolve(document.querySelector(selector));
+                observer.disconnect();
+                resolve(document.querySelector(selector));
             }
-
-            const observer = new MutationObserver(mutations => {
-                if (document.querySelector(selector)) {
-                    observer.disconnect();
-                    resolve(document.querySelector(selector));
-                }
-            });
-
-            // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
-            observer.observe(document.body, {
-                childList: true,
-                subtree: true
-            });
         });
-    }
 
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+async function start() {
     let visibleToAllSelected = true;
-    const commentBox = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form.organization-project-tasks-task-2-route__activity-form.js-organization-project-tasks-task-2-route-activity-form > form > div.comment-field.is-avatar-shown.activity-form__comment-field > div.comment-field__text-field-container.js-comment-field-text-field-container > div > div > div > div.trix-editor-wrapper > trix-editor');
-
+    const commentBox = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form._form_h8tMg.js-organization-project-tasks-task-2-route-activity-form > form > div.comment-field.is-avatar-shown.activity-form__comment-field > div.comment-field__text-field-container.js-comment-field-text-field-container > div > div.text-editor__editor-container');
     commentBox.addEventListener('click', async () => {
-        const visibilityToggleButton = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form.is-field-initially-focused.organization-project-tasks-task-2-route__activity-form.js-organization-project-tasks-task-2-route-activity-form > form > div.activity-form__actions-wrapper > button');
-
+        const visibilityToggleButton = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form.is-field-initially-focused._form_h8tMg.js-organization-project-tasks-task-2-route-activity-form > form > div.activity-form__actions-wrapper > button');
         if (visibleToAllSelected) {
             visibilityToggleButton.click();
             const hideFromClientButton = await waitForElm('#modal-container > div > div.popover-content__body-wrapper > div > div > div > button:nth-child(2)');
@@ -35,17 +33,15 @@ const start = async () => {
         }
     });
 
-    const commentBtn = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form.is-field-initially-focused.organization-project-tasks-task-2-route__activity-form.js-organization-project-tasks-task-2-route-activity-form > form > div.activity-form__actions-wrapper > div.form-actions.form__actions.activity-form__actions > button');
-
+    const commentBtn = await waitForElm('#fullscreen-modal-container > div > main > div > div.task-form-page__main > div.task-form-page__content > div.activity-form.is-field-initially-focused._form_h8tMg.js-organization-project-tasks-task-2-route-activity-form > form > div.activity-form__actions-wrapper > div.form-actions.form__actions.activity-form__actions > button');
     commentBtn.addEventListener('click', async () => {
         visibleToAllSelected = true;
     });
 
 }
 
+start();
+
 chrome.runtime.onMessage.addListener((request) => {
-    console.log(request);
     start();
 });
-
-start();
--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,10 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete' && tab.url.includes('app.productive.io')) {
-    chrome.tabs.sendMessage(tabId, {
-      message: 'ProductiveTabUpdated'
-    });
-    console.log('ProductiveTabUpdated Message Sent');
+    setTimeout(() => {
+      chrome.tabs.sendMessage(tabId, {
+        message: 'ProductiveTabUpdated'
+      });
+      console.log('ProductiveTabUpdated Message Sent');
+    }, 3000);
   }
 });
